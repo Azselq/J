5c1225610f9b97810f42007cfa38d6a5149238ef
Azselq <denok2002@gmail.com>
2021-12-11 18:24:53 +0300

    1

---
+++ Prac8/iWaitList.java
@@ -0,0 +1,10 @@
+package prac8;
+import java.util.Collection;
+public interface iWaitList<E> {
+    public void add(E element);
+    public E remove();
+    public boolean contains(E element);
+    public boolean containsAll(Collection<E> c);
+    public boolean isEmpty();
+}
+
